996fb329428bbc4253766e77ba4f382e2571e1a4
Jimpal1811 <jimpal2004@gmail.com>
2025-06-10 14:14:08 +0530

    Updas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,50 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   darkMode: false,
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     container: {
+//       center: true,
+//       padding: {
+//         DEFAULT: '0px',  // full screen layout
+//         lg: '0px',
+//       },
+//       screens: {
+//         'sm': {'min': '640px', 'max': '767px'},
+//         'md': {'min': '768px', 'max': '1023px'},
+//         'lg': {'min': '1024px', 'max': '1279px'},
+//         'xl': {'min': '1280px', 'max': '1535px'},
+//         '2xl': {'min': '1536px'},
+//       },
+//     },
+//     screens: {
+//       sm: '375px',
+//       md: '768px',
+//       lg: '1200px',
+//     },
+//     extend: {
+//       colors: {}
+//     },
+//   },
+//   plugins: [],
+// }
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: false,
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '0px',  // full screen layout
-        lg: '0px',
-      },
-      screens: {
-        'sm': {'min': '640px', 'max': '767px'},
-        'md': {'min': '768px', 'max': '1023px'},
-        'lg': {'min': '1024px', 'max': '1279px'},
-        'xl': {'min': '1280px', 'max': '1535px'},
-        '2xl': {'min': '1536px'},
-      },
-    },
-    screens: {
-      sm: '375px',
-      md: '768px',
-      lg: '1200px',
-    },
     extend: {
-      colors: {}
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        lobster: ['Lobster', 'cursive'],
+        pacifico: ['Pacifico', 'cursive'],
+      },
     },
   },
   plugins: [],
-}
+};
